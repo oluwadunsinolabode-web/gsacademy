fdec704a178e86bf7d951205e32f73eb42902665
Oluwadunsin Olabode <oluwadunsinsamuelolabode@gmail.com>
2026-08-08 06:46:40 +0100

    Fix student schedule editor import

--- app/admin-dashboard/students/[id]/edit/page.tsx
+++ app/admin-dashboard/students/[id]/edit/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
 import { getTutors } from "@/lib/services/tutor";
-import StudentScheduleEditor from "@/components/student-schedule-editor";
+import StudentScheduleEditor from "@/components/admin/student-schedule-editor";
 
 
 export default function EditStudentPage() {
